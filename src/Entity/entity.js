5264852227423ee717729772b395ecd0f05daac4
Noah Stinson <noahstjohn@ymail.com>
2021-05-26 22:39:29 -0500

    ai move to point moved to entity class

--- src/Entity/entity.js
+++ src/Entity/entity.js
@@ -8,4 +8,30 @@ export default class Entity {
         // throw new Error("Cannot create an instance of an abstract class");
     }
     // static id = 0;
+
+    // line information 
+    //  lineInfo = {pointsPath, lineLength, fraction}
+    move(lineInfo) {
+        // get point along line
+        const newPos = lineInfo.pointsPath.getPoint(lineInfo.fraction);
+        // move ai to that point
+        // aiSquare.renderObj.position.copy(newPos);
+        this.copyPos(newPos);
+
+        lineInfo.fraction += 0.5 / lineInfo.lineLength;
+        if (lineInfo.fraction > 1) {
+            lineInfo.pointsPath = null;
+        }
+    }
+
+    // vector = {x, y, z}
+    copyPos(vector) {
+        console.log(vector);
+        this.position.x = vector.x;
+        this.position.y = vector.y;
+        this.position.z = vector.z;
+
+        this.renderObj.position.copy(vector);
+    }
+
 }
