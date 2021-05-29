90306aefe0751d869a7aee3dee1e7f277de88ff9
Noah Stinson <noahstinson19@gmail.com>
2021-05-29 17:03:28 -0500

    added ray casting. obj is within group now

--- src/Entity/ai.js
+++ src/Entity/ai.js
@@ -2,19 +2,30 @@ import * as THREE from 'three';
 import Entity from './entity';
 
 export default class SquareAI extends Entity {
-        // color = 0x000000
-        // size = {width, height, depth}
-        // position = {x, y, z}
-    constructor(color, size, pos) {
-        super(color, size, pos);
+    // color = 0x000000
+    // size = {width, height, depth}
+    // position = {x, y, z}
+    constructor(color, size, pos, scene) {
+        super(color, size, pos, scene);
 
         const aiGeometry = new THREE.BoxGeometry(size.width, size.height, size.depth);
         const aiMaterial = new THREE.MeshBasicMaterial();
         aiMaterial.color = new THREE.Color(color);
 
         this.renderObj = new THREE.Mesh(aiGeometry, aiMaterial);
-        this.renderObj.position.x = pos.x;
-        this.renderObj.position.y = pos.y;
-        this.renderObj.position.z = pos.z;
+        this.group.position.x = pos.x;
+        this.group.position.y = pos.y;
+        this.group.position.z = pos.z;
+        this.group.add(this.renderObj);
+
+        // adding rays to object
+        this.addRay(this.group.position, new THREE.Vector3(0, 1, 0)); // up
+        this.addRay(this.group.position, new THREE.Vector3(1, 1, 0)); // up right
+        this.addRay(this.group.position, new THREE.Vector3(1, 0, 0)); // right
+        this.addRay(this.group.position, new THREE.Vector3(1, -1, 0)); // down right
+        this.addRay(this.group.position, new THREE.Vector3(0, -1, 0)); // down 
+        this.addRay(this.group.position, new THREE.Vector3(-1, -1, 0)); // down left
+        this.addRay(this.group.position, new THREE.Vector3(-1, 0, 0)); // left
+        this.addRay(this.group.position, new THREE.Vector3(-1, 1, 0)); // up left
     }
 }
