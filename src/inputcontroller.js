ac155eea6eebf97dd59b362c832738e159a4563f
Noah Stinson <noahstjohn@ymail.com>
2021-06-21 18:28:02 -0500

    input controller for future

---
+++ src/inputcontroller.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+import Entity from './entity';
+import Node from './node.js';
+
+export default class InputController {
+    // color = 0x000000
+    // size = {width, height, depth}
+    // position = {x, y, z}
+
+    constructor(canvas) {
+        this.canvas = canvas;
+    }
+
+}
